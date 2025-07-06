d3f943a93c931fdaeeba18f8fba6cecb54beaacd
Ilya <great.mars15@gmail.com>
2025-07-06 22:05:11 +0300

    updated

--- server.js
+++ server.js
@@ -4,4 +4,7 @@ const server = http.createServer((req, res) => {
     res.end("DEMO NODE JS APP TO TEST DOCKER!");
 });
 
-server.listen(3000, "0.0.0.0", () =>  {});
+server.listen(3000, "0.0.0.0", () =>  {
+   console.log("server is listening on:");
+   console.log("http://localhost:3000");
+});
